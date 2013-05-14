29bcda4325707c525e0bcbddfbe3876e41238b78
G. D. McBain <gmcbain@memjet.com>
2013-05-14 07:20:01 +0000

    dysys.LinearDySys: allowed constraining degrees of freedom by mapping
    git-svn-id: http://svn-micro-design/06-micro-design/trunk/48-Software_Tools_Source_Code/46-DySys/DySys@44297 bac33084-4802-0410-af3f-d44b037110af

--- dysys/linear_dysys.py
+++ dysys/linear_dysys.py
@@ -1,3 +1,16 @@
+#!/usr/bin/env python
+
+'''a module for linear 'descriptor' systems
+
+:author: G. D. McBain <gmcbain>
+
+:created: 2013-01-11
+
+'''
+
+import numpy as np
+from scipy.sparse import identity
+
 from dysys import DySys
 
 class LinearDySys(DySys):
@@ -18,3 +31,50 @@ class LinearDySys(DySys):
 
         self.M, self.D, self.f = M, D, f
 
+    def __len__(self):
+        return self.D.shape[0]
+
+    def node_maps(self, known):
+        '''return the matrices mapping the unknown and knowns
+
+        to the global nodes.
+
+        This concerns the imposition of nodal degree-of-freedom
+        constraints as inspired by the comments of Roy Stogner in the
+        Libmesh-users list thread "interaction between subdomain_id
+        and dof constraints?"  (2012-02-25).  The idea is to represent
+        the column vector of all unknowns x as U * xu + K * xk, where
+        xu are unknown and xk are known whose lengths together add to
+        that of x and U and K are rectangular matrices, typically
+        columns of the identity.
+
+        '''
+
+        # KLUDGE: gmcbain 2013-01-29: I don't know how to deal with
+        # arrays with zero rows or columns in scipy.sparse, so I need
+        # to treat it as a special case.  Yuck.  GNU Octave does the
+        # obvious right thing.  I think the problem applies to NumPy
+        # too.
+        
+        if len(known) > 0:
+            return [identity(len(self))[:,c] for c in
+                    (np.setdiff1d(np.arange(len(self)), known), known)]
+        else:
+            return [identity(self.nodes), None]
+
+    def constrain(self, known, xknown, vknown=None):
+        
+        U, K = self.node_maps(known)
+        (M, D) = [U.T * A * U for A in [self.M, self.D]]
+        sys = self.__class__(M, D,
+                             lambda t: U.T * (self.f(t) -
+                                              self.D * K * xknown -
+                                              (0 if vknown is None else
+                                               self.M * K * vknown)))
+        sys.U, sys.K, sys.xknown = U, K, xknown
+        return sys
+
+    def reconstitute(self, x):
+        "don't try this except on systems returned by constrain"
+        return self.U * x + self.K * self.xknown
+        
