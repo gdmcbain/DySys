fd1de4803d13a873bc7ffeb9b3875e40edb95873
G. D. McBain <gmcbain@memjet.com>
2013-08-05 23:04:00 +0000

    pep8
    git-svn-id: http://svn-micro-design/06-micro-design/trunk/48-Software_Tools_Source_Code/46-DySys/DySys@44884 bac33084-4802-0410-af3f-d44b037110af

--- setup.py
+++ setup.py
@@ -9,10 +9,11 @@ setup(
     scripts=[],
     url='http://swiki/display/memjet/DySys',
     license='LICENSE.txt',
-    description='some code for dynamical systems, particularly those of the descriptor type',
+    description='''some code for dynamical systems,
+particularly those of the descriptor type''',
     long_description=open('README.txt').read(),
     install_requires=[
-        'numpy', 
-        'scipy.sparse'
+        'numpy',
+        'scipy'
         ],
 )
