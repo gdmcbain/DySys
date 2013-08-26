578d83fc34e1388e9bc1a6f80f05d87fdf2cdd1b
G. D. McBain <gmcbain@memjet.com>
2013-08-26 04:58:21 +0000

    moved store_last from 6-48/35-Millihydraulics to 6-48/46-DySys
    git-svn-id: http://svn-micro-design/06-micro-design/trunk/48-Software_Tools_Source_Code/46-DySys/DySys@45041 bac33084-4802-0410-af3f-d44b037110af

--- dysys/__init__.py
+++ dysys/__init__.py
@@ -2,4 +2,5 @@ from dysys import DySys
 from fixed_point import (fixed_point, newton)
 from sparse_dysys import SparseDySys
 from sparsenf_dysys import SparseNFDySys
+from store_last import StoreLast
 from newmark import Newmark
---
+++ dysys/store_last.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+'''
+:author: G. D. McBain <gmcbain>, based on code read 2013-06-11
+http://www.dabeaz.com/generators-uk/storelast.py, Copyright (C) 2008
+David M. Beazley with no licensing information
+
+:created: 2013-06-13
+
+'''
+
+
+class StoreLast(object):
+    'wrap an iterable to remember the last item generated'
+
+    def __init__(self, source):
+        self.source = source
+
+    def next(self):
+        self.last = self.source.next()
+        return self.last
+
+    def __iter__(self):
+        return self
