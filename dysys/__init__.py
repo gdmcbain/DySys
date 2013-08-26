b7846a4b1df622c97012abd4259170293e96f6f2
G. D. McBain <gmcbain@memjet.com>
2013-08-26 03:46:20 +0000

    dysys.__init__.py: updated to reflect r45035
    git-svn-id: http://svn-micro-design/06-micro-design/trunk/48-Software_Tools_Source_Code/46-DySys/DySys@45037 bac33084-4802-0410-af3f-d44b037110af

--- dysys/__init__.py
+++ dysys/__init__.py
@@ -1,5 +1,5 @@
 from dysys import DySys
-from fixed_point import (FixedPoint, newton)
+from fixed_point import (fixed_point, newton)
 from sparse_dysys import SparseDySys
 from sparsenf_dysys import SparseNFDySys
 from newmark import Newmark
