657964a7be27c4687ed20e861a864784e6feb73c
G. D. McBain <gmcbain@memjet.com>
2013-08-26 01:38:26 +0000

    dysys.fixed_point: doc fix
    git-svn-id: http://svn-micro-design/06-micro-design/trunk/48-Software_Tools_Source_Code/46-DySys/DySys@45026 bac33084-4802-0410-af3f-d44b037110af

--- dysys/fixed_point.py
+++ dysys/fixed_point.py
@@ -18,6 +18,22 @@ class FixedPoint(object):
 
     def __init__(self, iteration,
                  tol=np.MachAr().eps, maxiter=np.iinfo(np.int).max):
+
+        '''
+
+        :param: iteration, iterable generating pairs of values and
+        convergence test values
+
+        :param: tol, positive float
+
+        :param: maxiter, positive integer
+
+        When called (without arguments), the object returns the first
+        element of the first pair for which the second value is less
+        than tol (in the sense of numpy.linalg.norm)
+
+        '''
+
         self.iteration = iteration
         self.tol = tol
         self.maxiter = maxiter
@@ -38,7 +54,23 @@ class FixedPoint(object):
 
 
 def newton(residual, jacobian, x, *args, **kwargs):
-    'eliminate the residual by Newton-iteration'
+    '''eliminate the residual by Newton-iteration
+
+    :param: residual, a function taking a tuple with first term a
+    one-dimensional numpy.array to another array of the same len
+
+    :param: jacobian, a function like residual but returning a square
+    numpy.array of corresponding shape
+
+    :param: x, a tuple with first term a one-dimensional numpy.array
+    of the length expected by the residual and jacobian functions
+
+    Any other positional or keyword arguments are passed on to the
+    FixedPoint constructor; of particular interest are tol and
+    maxiter.
+
+    '''
+
     def iteration(x):
         while True:
             try:
