e4f6728f8efbd8e40726f1a1616ccbe667267bee
G. D. McBain <gmcbain@memjet.com>
2013-08-26 01:54:30 +0000

    dysys.fixed_point: exported newton function
    git-svn-id: http://svn-micro-design/06-micro-design/trunk/48-Software_Tools_Source_Code/46-DySys/DySys@45029 bac33084-4802-0410-af3f-d44b037110af

--- dysys/__init__.py
+++ dysys/__init__.py
@@ -1,5 +1,5 @@
 from dysys import DySys
-from fixed_point import FixedPoint
+from fixed_point import (FixedPoint, newton)
 from sparse_dysys import SparseDySys
 from sparsenf_dysys import SparseNFDySys
 from newmark import Newmark
