1b1a03bb332242c1dabde5ded850cbf2ce210479
G. D. McBain <gmcbain@memjet.com>
2014-03-14 05:33:21 +0000

    rearranged demo (relative import in non-package and all that
    git-svn-id: http://svn-micro-design/06-micro-design/trunk/48-Software_Tools_Source_Code/46-DySys/DySys@45852 bac33084-4802-0410-af3f-d44b037110af

---
+++ dysys/demo/sparse_dysys_demo.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+'''
+:author: G. D. McBain <gmcbain>
+
+:created: 2014-03-14
+
+'''
+
+from __future__ import absolute_import, division, print_function
+
+from dysys.sparse_dysys import main
+
+if __name__ == '__main__':
+    main()
