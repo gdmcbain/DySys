6e4bcae1f90118a6200f57f9e9282002c2e0c6c0
G. D. McBain <gmcbain@memjet.com>
2014-05-08 04:47:48 +0000

    DOC
    git-svn-id: http://svn-micro-design/06-micro-design/trunk/48-Software_Tools_Source_Code/46-DySys/DySys@46214 bac33084-4802-0410-af3f-d44b037110af

--- dysys/test/test_steady_sparse.py
+++ dysys/test/test_steady_sparse.py
@@ -23,6 +23,31 @@ from dysys import SparseDySys
 
 class TestLadder(TestCase):
 
+    ''' Consider the three-cell ladder network with (8) nodes and [10]
+    equal branches, with one corner node (0) vented and the one
+    diagonally opposite (7) at a constant suction of -5 kPa.
+
+    .|--(0)-[6]-(4)
+         |       |
+        [0]     [3]
+         |       |
+        (1)-[7]-(7)
+         |       |
+        [1]     [4]
+         |       |
+        (2)-[8]-(7)
+         |       |
+        [2]     [5]
+         |       |
+        (3)-[9]-(7)=<-5 kPa>-|.
+
+    In the exact solution, nodes (4) and (1) have pressures -1 and -5
+    / 3.
+        
+
+    '''
+
+
     @classmethod
     def setUpClass(cls):
         nodes = 8
