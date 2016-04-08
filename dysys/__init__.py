abacd4dd9ec48ba364e3b5b61c28f2c11838ffcb
G. D. McBain <gmcbain@memjet.com>
2016-04-08 06:16:17 +0000

    Summary: exported HilberHughesTaylor
    git-svn-id: http://svn-micro-design/06-micro-design/trunk/48-Software_Tools_Source_Code/46-DySys/DySys@48513 bac33084-4802-0410-af3f-d44b037110af

--- dysys/__init__.py
+++ dysys/__init__.py
@@ -1,8 +1,9 @@
 from .dysys import DySys, node_maps
-from .fixed_point import (fixed_point, newton)
+from .fixed_point import fixed_point, newton
 from .sparse_dysys import SparseDySys
 from .nonlinear_sparse_dysys import NonlinearSparseDySys
 from .sparsenf_dysys import SparseNFDySys
 from .store_last import StoreLast
 from .newmark import Newmark
+from .hilber_hughes_taylor import HilberHughesTaylor
 from .odysys import ODySys
