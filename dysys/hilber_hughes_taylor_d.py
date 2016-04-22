675306df8f1e010acb697b0b8ef81bb40bc6da06
G. D. McBain <gmcbain@memjet.com>
2016-04-22 02:49:17 +0000

    Summary: new file
    git-svn-id: http://svn-micro-design/06-micro-design/trunk/48-Software_Tools_Source_Code/46-DySys/DySys@48543 bac33084-4802-0410-af3f-d44b037110af

---
+++ dysys/hilber_hughes_taylor_d.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''This implements the Hilber-Hughes-Taylor (a.k.a. alpha-) method
+
+(Hilber 1976; Hilber, Hughes, & Taylor 1977; Cook, Malkus, & Plesha
+1989, p. 409; Hughes 2000, p. 532; Craveur 2008, §18.4.5), as a
+generalization of the Newmark method
+
+:author: G. D. McBain <gmcbain>
+
+:created: 2016-04-21
+
+'''
+
+from __future__ import absolute_import, division, print_function
+
+from .dysys import DySys
+from .fixed_point import solve
+
+
+class HilberHughesTaylor_d(DySys):
+    '''a dynamical system advancing with the alpha-method in d-form
+
+    See also: HilberHughesTaylor
+
+    '''
+
+    def __init__(self, M, C, K, f, alpha=0.):
+
+        ''':param M: mass scipy.sparse matrix
+
+        :param C: damping scipy.sparse matrix
+
+        :param K: stiffness scipy.sparse
+
+        :param f: function of time and dict of discrete dynamical
+        variables, returning forcing vector
+
+        :param alpha: float, Hilber-Hughes-Taylor method parameter,
+        should be in [-1/3, 0]
+
+        '''
+
+        self.M, self.C, self.K = M, C, K
+        self.f = f
+        self.alpha = alpha
+        self.beta = (1 - alpha)**2 / 4.
+        self.gamma = (1 - 2 * alpha) / 2.
+
+    def setKeff(self, h):
+        self.Keff = (self.M / self.beta / h**2 +
+                     (1 + self.alpha) * (self.gamma * self.C / self.beta / h +
+                                         self.K))
+
+    def step(self, t, h, x, d):
+        'evolve from displacement x at time t to t+h'
+
+        x1 = solve(self.Keff,
+                   (1 + self.alpha) * self.f(t + h, d) -
+                   self.alpha * self.f(t, d) +
+                   self.M.dot(x / self.beta / h**2 +
+                              self.v / self.beta / h +
+                              (1 - 2 * self.beta) / 2. / self.beta * self.a) +
+                   self.C.dot((1 + self.alpha) * self.gamma /
+                              self.beta / h * x +
+                              ((1 + self.alpha) * self.gamma / self.beta - 1) *
+                              self.v -
+                              (1 + self.alpha) *
+                              (1 - self.gamma / 2. / self.beta) * h * self.a) +
+                   self.K.dot(self.alpha * x))
+
+        a1 = ((x1 - x - h * self.v) / self.beta / h**2 -
+              (.5 / self.beta - 1)  * self.a)
+        self.v = self.v + h * ((1 - self.gamma) * self.a + self.gamma * a1)
+        
+        self.a = a1
+
+        return x1
+
+    def march(self, h, x, d=None, *args, **kwargs):
+
+        d = {} if d is None else d
+
+        self.v = x[1]
+
+        self.a = solve(self.M,
+                       self.f(0., d) - self.C.dot(x[1]) - self.K.dot(x[0]))
+
+        self.setKeff(h)
+
+        if 'f' not in kwargs:
+            # TRICKY gmcbain 2016-04-08: Return the rate of change of
+            # the solution too
+            kwargs['f'] = lambda x: (x, self.v)
+
+        return super(self.__class__, self).march(h, x[0], d, *args, **kwargs)
