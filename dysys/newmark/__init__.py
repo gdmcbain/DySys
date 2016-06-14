1f93cb877d77c9768ff59399f56c2724bb3b7896
G. D. McBain <gmcbain@memjet.com>
2016-06-14 00:18:55 +0000

    Summary: exported special versions of Newmark
    git-svn-id: http://svn-micro-design/06-micro-design/trunk/48-Software_Tools_Source_Code/46-DySys/DySys@48861 bac33084-4802-0410-af3f-d44b037110af

--- dysys/newmark/__init__.py
+++ dysys/newmark/__init__.py
@@ -1,2 +1,6 @@
 from .newmark import Newmark
+from .newmark import (trapezoidal,
+                      fox_goodwin,
+                      linear_acceleration,
+                      central_difference)
 from .hilber_hughes_taylor import HilberHughesTaylor
