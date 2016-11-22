688e299f20794801d956a5f637b1950f4a5a3e56
G. D. McBain <gmcbain@memjet.com>
2016-11-22 22:59:11 +0000

    Summary: bumped version to 0.2.0
    git-svn-id: http://svn-micro-design/06-micro-design/trunk/48-Software_Tools_Source_Code/46-DySys/DySys@49826 bac33084-4802-0410-af3f-d44b037110af

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup, Command
 
 setup(
     name='DySys',
-    version='0.1.0',
+    version='0.2.0',
     author='G. D. McBain',
     author_email='gmcbain@memjet.com',
     packages=['dysys',
