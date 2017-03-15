f65d96e2af624d28b7cb0676cd65c1c0f7de8681
G. D. McBain <gmcbain@memjet.com>
2017-03-15 06:31:19 +0000

    Summary: DOC
    git-svn-id: http://svn-micro-design/06-micro-design/trunk/48-Software_Tools_Source_Code/46-DySys/DySys@50247 bac33084-4802-0410-af3f-d44b037110af

--- dysys/store_last.py
+++ dysys/store_last.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-'''
+'''Deprecated class to extract last item of a sequence.
+
+Prefer toolz.itertoolz.last.
+
 :author: G. D. McBain <gmcbain>, based on code read 2013-06-11
 http://www.dabeaz.com/generators-uk/storelast.py, Copyright (C) 2008
 David M. Beazley with no licensing information
@@ -16,6 +20,7 @@ class StoreLast(object):
     'wrap an iterable to remember the last item generated'
 
     def __init__(self, source):
+        raise DeprecationWarning('Just use toolz.itertoolz.last.')
         self.source = source
 
     def __next__(self):
