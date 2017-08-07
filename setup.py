468fe2aad681ec652b5db68d535cb6be675e9466
G. D. McBain <gmcbain@memjet.com>
2017-08-07 16:42:05 +1000

    bumped version number again

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='DySys',
-    version='0.3.2',
+    version='0.3.3',
     author='G. D. McBain',
     author_email='gmcbain@memjet.com',
     packages=['dysys',
