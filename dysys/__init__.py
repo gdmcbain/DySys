d2d4cc2a88a6affa80780a500b2047c7f7cace44
G. D. McBain <gmcbain@memjet.com>
2017-09-26 15:46:58 +1000

    new class AlgebraicDySys

--- dysys/__init__.py
+++ dysys/__init__.py
@@ -3,6 +3,7 @@ from .signal_flow_path_sys import SignalFlowPathSys
 from .linear_dysys import (ScalarLinearDySys, SparseDySys,
                            NonlinearSparseDySys, SparseNFDySys)
 from .odysys import ODySys
+from .algebraic_dysys import AlgebraicDySys
 from .uncoupled_dysys import UncoupledDySys
 from .newmark import Newmark, HilberHughesTaylor
 
