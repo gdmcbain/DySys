fb68d0101a711de65544bba3f66c51a3d49d0da6
G. D. McBain <gmcbain@memjet.com>
2018-03-22 14:01:56 +1100

    updated Build.hs to use latest goodies from shake-memjet-extras

--- examples/newmark/Build.hs
+++ examples/newmark/Build.hs
@@ -1,22 +1,17 @@
 #!/usr/bin/env stack
-{- stack runhaskell
+{-stack runhaskell
  --package shake
  -}
 
 import Development.Shake
 import Development.Shake.FilePath
+import Development.Shake.Clean
 
 main :: IO ()
 main = shakeArgs shakeOptions $ do
   want ["ph.png"]
 
-  "clean" ~> do
-    need ["hlint", "PEP-8"]
-    removeFilesAfter "." ["msm*", "*~", "*.png"]
-
-  "hlint" ~> cmd "hlint" "."
-
-  "PEP-8" ~> cmd "flake8" "."
+  cleanRules ["*.png"]
 
   "*.png" %> \png -> do
     let script = png -<.> "py"
