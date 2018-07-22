512797b4757334bd078918451a51a9b8fb50d9e5
G. D. McBain <gmcbain@memjet.com>
2018-07-23 09:25:10 +1000

    DEV: test_requires -> tests_require

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='DySys',
-    version='0.3.7',
+    version='0.4.0',
     author='G. D. McBain',
     author_email='gmcbain@memjet.com',
     packages=['dysys',
@@ -13,10 +13,14 @@ setup(
     description='''some code for dynamical systems,
 particularly those of the descriptor type''',
     long_description=open('README.md').read(),
-    setup_requires=['numpy'],
-    install_requires=['numpy',
+    python_requires='>=3.6',
+    setup_requires=['pytest-runner', 'pytest-xdist'],
+    tests_require=['pytest'],
+    install_requires=['attrs',
+                      'toolz',
+                      'numpy',
                       'scipy',
                       'pandas',
                       'scikit-sparse;platform_system!="Windows"',
-                      'toolz']
+    ]
 )
